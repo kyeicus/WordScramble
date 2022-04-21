ca8fd535504c81b4789214036655835dec50cafc
Emmanuel Kyei Nketia <eknketia001@st.ug.edu.gh>
2022-04-21 18:13:46 +0000

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Emmanuel K. Nketia on 4/21/22.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
